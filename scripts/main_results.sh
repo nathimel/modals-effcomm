f8fe8bf6f6c583c8922837d1aaf2cb916de1ed39
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-03-20 20:25:32 -0700

    if guard on args

--- scripts/main_results.sh
+++ scripts/main_results.sh
@@ -3,18 +3,25 @@
 # Usage: ./scripts/main_results/run.sh path-to-config  
 # Example: ./scripts/main_results/run.sh configs/main_results/config.yml 
 
-$CONFIG = $1
+if test $# -lt 1
+then
+    echo "Usage: ./scripts/main_results/run.sh path-to-config"
+    exit 1
+fi
 
-python3 build_meaning_space.py $CONFIG
 
-python3 generate_expressions.py $CONFIG
+CONFIG=$1
 
-python3 sample_languages.py $CONFIG
+python3 src/build_meaning_space.py $CONFIG #not obvious this is necessary
 
-python3 add_natural_languages.py $CONFIG
+python3 src/generate_expressions.py $CONFIG
 
-python3 estimate_pareto_frontier.py $CONFIG
+python3 src/sample_languages.py $CONFIG
 
-python3 measure_languages.py $CONFIG
+python3 src/add_natural_languages.py $CONFIG
 
-python3 analysis.py $CONFIG
+python3 src/estimate_pareto_frontier.py $CONFIG
+
+python3 src/measure_languages.py $CONFIG
+
+python3 src/analysis.py $CONFIG
