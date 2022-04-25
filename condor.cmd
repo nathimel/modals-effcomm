5508ff07848187699f5dd8f34c1e21a9b4b792b7
Nathaniel <nimel@gmail.com>
2022-04-24 19:38:07 -0700

    ran on dryas to completion

---
+++ condor.cmd
@@ -0,0 +1,9 @@
+executable = run.sh
+getenv     = true
+output        = run.out
+error      = run.error
+log        = run.log
+notification = complete
+transfer_executable = false
+request_memory = 2*1024
+queue
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-source ~/miniforge3/etc/profile.d/conda.sh
+# source ~/miniforge3/etc/profile.d/conda.sh
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate modals-effcomm
 
 ./scripts/run_full_experiment.sh configs/dev.yml
