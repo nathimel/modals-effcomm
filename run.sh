dc3d8fe733fde86730b6c9a1456c68b21402bbb1
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-04-29 13:27:33 -0700

    ran main results

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ conda activate modals-effcomm
 # ./scripts/run_full_experiment.sh configs/dev_1_4.yml
 # ./scripts/run_full_experiment.sh configs/dev_3_3.yml
 # ./scripts/run_full_experiment.sh configs/dev_2_5.yml
-./scripts/run_full_experiment.sh configs/dev.yml
-# ./scripts/run_full_experiment.sh configs/main_results.yml
+# ./scripts/run_full_experiment.sh configs/dev.yml
+./scripts/run_full_experiment.sh configs/main_results.yml
 
 conda deactivate
