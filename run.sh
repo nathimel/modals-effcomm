2d99f051473d1f60d716bfe0090204d825a7c90d
Nathaniel <nimel@gmail.com>
2022-05-05 15:31:26 -0700

    ran 4 combos of utility and informativity on condor

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-source ~/miniforge3/etc/profile.d/conda.sh # Local
-# source ~/anaconda3/etc/profile.d/conda.sh # Patas
+# source ~/miniforge3/etc/profile.d/conda.sh # Local
+source ~/anaconda3/etc/profile.d/conda.sh # Patas
 conda activate modals-effcomm
 
 # ./scripts/run_full_experiment.sh configs/dev_4_4.yml
