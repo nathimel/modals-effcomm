26396c1ff4e29aec304345532f24e072c858d495
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-05-07 14:52:01 -0700

    statistics not promising

--- src/create_folders.py
+++ src/create_folders.py
@@ -3,17 +3,22 @@
 import sys
 from misc.file_util import load_configs, make_path
 
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 src/create_folers.py path_to_config_file")
-        raise TypeError(f"Expected {2} arguments but received {len(sys.argv)}.")        
+        raise TypeError(f"Expected {2} arguments but received {len(sys.argv)}.")
 
     config_fn = sys.argv[1]
     configs = load_configs(config_fn)
 
-    for file_key in configs['file_paths']:
+    for file_key in configs["file_paths"]:
         if file_key != "data":
-            make_path(configs['file_paths'][file_key])
+            if file_key == "analysis":
+                for file_key_ in configs["file_paths"][file_key]:
+                    make_path(configs["file_paths"][file_key][file_key_])
+            else:
+                make_path(configs["file_paths"][file_key])
 
 
 if __name__ == "__main__":
