386ac8b40f19e429ea2dddfa5840812e7357ebf7
Nathaniel <nimel@gmail.com>
2022-05-09 10:54:19 -0700

    an condor

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-source ~/miniforge3/etc/profile.d/conda.sh # Local
-# source ~/anaconda3/etc/profile.d/conda.sh # Patas
+# source ~/miniforge3/etc/profile.d/conda.sh # Local
+source ~/anaconda3/etc/profile.d/conda.sh # Patas
 conda activate modals-effcomm
 
 # ./scripts/run_full_experiment.sh configs/dev_4_4.yml
@@ -12,9 +12,9 @@ conda activate modals-effcomm
 # ./scripts/run_full_experiment.sh configs/dev.yml
 # ./scripts/run_full_experiment.sh configs/main_results.yml
 
-./scripts/run_full_experiment.sh configs/indicator_literal.yml > outputs/indicator_literal.out
-./scripts/run_full_experiment.sh configs/indicator_pragmatic.yml > outputs/indicator_pragmatic.out
-./scripts/run_full_experiment.sh configs/half_credit_literal.yml > outputs/half_credit_literal.out
-./scripts/run_full_experiment.sh configs/half_credit_pragmatic.yml > outputs/half_credit_pragmatic.out
+./scripts/run_full_experiment.sh configs/indicator_literal.yml > outputs/indicator_literal/system_output.txt
+./scripts/run_full_experiment.sh configs/indicator_pragmatic.yml > outputs/indicator_pragmatic/system_output.txt
+./scripts/run_full_experiment.sh configs/half_credit_literal.yml > outputs/half_credit_literal/system_output.txt
+./scripts/run_full_experiment.sh configs/half_credit_pragmatic.yml > outputs/half_credit_pragmatic/system_output.txt
 
 conda deactivate
