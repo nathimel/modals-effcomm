6332d784683a4c03a1563240d7add5a4fefb3112
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-10-19 16:46:33 -0700

    full exp pipeline runs smoothly

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ source ~/miniforge3/etc/profile.d/conda.sh # Local
 # source ~/anaconda3/etc/profile.d/conda.sh # Patas
 conda activate modals-effcomm
 
-time ./scripts/run_full_experiment.sh configs/dev.yml
+# time ./scripts/run_full_experiment.sh configs/dev.yml
 # time ./scripts/run_full_experiment.sh configs/salt.yml > outputs/salt/system_output.txt
 
 # time ./scripts/run_full_experiment.sh configs/indicator_literal.yml > outputs/indicator_literal/system_output.txt
 # time ./scripts/run_full_experiment.sh configs/indicator_pragmatic.yml > outputs/indicator_pragmatic/system_output.txt
-# time ./scripts/run_full_experiment.sh configs/half_credit_literal.yml > outputs/half_credit_literal/system_output.txt
+time ./scripts/run_full_experiment.sh configs/half_credit_literal.yml > outputs/half_credit_literal/system_output.txt
 # time ./scripts/run_full_experiment.sh configs/half_credit_pragmatic.yml > outputs/half_credit_pragmatic/system_output.txt
 
 
