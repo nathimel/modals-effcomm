f91c2fdc10b1ec229a3fce5f5e4fa274fc7b67c9
Nathaniel <nimel@gmail.com>
2022-10-25 11:30:57 -0700

    ran uniform

--- src/extract_prior.py
+++ src/extract_prior.py
@@ -85,96 +85,101 @@ def main():
     space = file_util.load_space(configs["file_paths"]["meaning_space"])
     modality_corpus = configs["file_paths"]["modality_corpus"]
 
-    ##########################################################################
-    # Load and parse corpus to extract auxiliaries
-    ##########################################################################
+    print("Constructing uniform prior")
+    prior = generate_uniform(space)
 
-    print("Loading dataframes...")
-    # list of filenames of each dataset used
-    fns = [fn for folder in modality_corpus for fn in modality_corpus[folder].values()]
-    # load dataframe of all files concatenated    
-    df_all = pd.concat(renumber_sentences([pd.read_csv(fn, sep="\t") for fn in fns]))
+    if False: # TODO: move option for uniform/estimated to config file
 
-    # for dev
-    # df_all = df_all.head(n=1000)
+        ##########################################################################
+        # Load and parse corpus to extract auxiliaries
+        ##########################################################################
 
-    # run sentences through a parser to extract the verbal auxiliaries
-    nlp = spacy.load("en_core_web_md")
+        print("Loading dataframes...")
+        # list of filenames of each dataset used
+        fns = [fn for folder in modality_corpus for fn in modality_corpus[folder].values()]
+        # load dataframe of all files concatenated    
+        df_all = pd.concat(renumber_sentences([pd.read_csv(fn, sep="\t") for fn in fns]))
 
-    # Run spacy pipeline on all sentences from full dataframe
-    sentence_id_max = df_all["sentence_id"].max()
-    dataset = [
-        list(df_all[df_all["sentence_id"] == i]["token"])
-        for i in range(sentence_id_max + 1)
-    ]  # a list of lists
+        # for dev
+        # df_all = df_all.head(n=1000)
 
-    print("Parsing and tagging sentences...")
-    docs = [nlp(Doc(nlp.vocab, sent)) for sent in tqdm(dataset)]  # this takes about 6m
+        # run sentences through a parser to extract the verbal auxiliaries
+        nlp = spacy.load("en_core_web_md")
 
-    # inject tags back as columns for the dataframe
-    pos_tags = [tok.pos_ for doc in docs for tok in doc]
-    df_all["POS"] = pos_tags
+        # Run spacy pipeline on all sentences from full dataframe
+        sentence_id_max = df_all["sentence_id"].max()
+        dataset = [
+            list(df_all[df_all["sentence_id"] == i]["token"])
+            for i in range(sentence_id_max + 1)
+        ]  # a list of lists
 
-    # Subset just the verbal auxiliaries
-    df_aux = df_all[df_all["POS"] == "AUX"]
+        print("Parsing and tagging sentences...")
+        docs = [nlp(Doc(nlp.vocab, sent)) for sent in tqdm(dataset)]  # this takes about 6m
 
-    # Correct typos, e.g. S-knowledge to S_knowledge
-    df_aux["gold_modal"] = df_aux["gold_modal"].str.replace(
-        r"([a-z])\-([a-z])", r"\1_\2", n=0, case=False
-    )
+        # inject tags back as columns for the dataframe
+        pos_tags = [tok.pos_ for doc in docs for tok in doc]
+        df_all["POS"] = pos_tags
 
-    # lowercase all auxiliaries to collapse e.g, Can and can
-    df_aux["token"] = df_aux["token"].str.lower()
+        # Subset just the verbal auxiliaries
+        df_aux = df_all[df_all["POS"] == "AUX"]
 
-    ##########################################################################
-    # Estimate relative frequencies of flavors
-    ##########################################################################
-
-    # replace gold_modal annotations with our flavors
-    for label in labels_to_flavors:
+        # Correct typos, e.g. S-knowledge to S_knowledge
         df_aux["gold_modal"] = df_aux["gold_modal"].str.replace(
-            label, labels_to_flavors[label]
+            r"([a-z])\-([a-z])", r"\1_\2", n=0, case=False
         )
 
-    # drop any flavors not belonging, e.g. S_agent
-    df_aux = df_aux[df_aux["gold_modal"].isin(labels_to_flavors.values())]
+        # lowercase all auxiliaries to collapse e.g, Can and can
+        df_aux["token"] = df_aux["token"].str.lower()
 
-    # annotate forces by mapping a token to its force label
-    token_dfs = []
-    for token in token_to_force:
-        df_token = df_aux[df_aux["token"] == token]
-        df_token["force"] = token_to_force[token]
-        token_dfs.append(df_token)
+        ##########################################################################
+        # Estimate relative frequencies of flavors
+        ##########################################################################
 
-    df_force_flavor = pd.concat(token_dfs)
-    df_force_flavor = df_force_flavor.rename(columns={"gold_modal": "flavor"})
+        # replace gold_modal annotations with our flavors
+        for label in labels_to_flavors:
+            df_aux["gold_modal"] = df_aux["gold_modal"].str.replace(
+                label, labels_to_flavors[label]
+            )
 
-    # Get number counts for force-flavor combinations
-    # Note that the meaning space must be perfectly compatible with our hand-annotations
-    point_counts = {point.name: 0 for point in space.referents}
-    points_dfs = []
-    for force, flavor in itertools.product(space.forces, space.flavors):
-        # find occurrences of the meaning point in the data
-        df_point = df_force_flavor[
-            (df_force_flavor["force"] == force) & (df_force_flavor["flavor"] == flavor)
-        ]
-        points_dfs.append(df_point)
-        # update our counts
-        point = f"{force}+{flavor}"
-        point_counts[point] = len(df_point)
+        # drop any flavors not belonging, e.g. S_agent
+        df_aux = df_aux[df_aux["gold_modal"].isin(labels_to_flavors.values())]
 
-    # Save a minimal dataframe of counts for later easy reference
-    df_points = pd.concat(points_dfs)
-    df_points = df_points.drop(columns=["sentence_id", "POS"])
+        # annotate forces by mapping a token to its force label
+        token_dfs = []
+        for token in token_to_force:
+            df_token = df_aux[df_aux["token"] == token]
+            df_token["force"] = token_to_force[token]
+            token_dfs.append(df_token)
 
-    total = sum(point_counts.values())
+        df_force_flavor = pd.concat(token_dfs)
+        df_force_flavor = df_force_flavor.rename(columns={"gold_modal": "flavor"})
+
+        # Get number counts for force-flavor combinations
+        # Note that the meaning space must be perfectly compatible with our hand-annotations
+        point_counts = {point.name: 0 for point in space.referents}
+        points_dfs = []
+        for force, flavor in itertools.product(space.forces, space.flavors):
+            # find occurrences of the meaning point in the data
+            df_point = df_force_flavor[
+                (df_force_flavor["force"] == force) & (df_force_flavor["flavor"] == flavor)
+            ]
+            points_dfs.append(df_point)
+            # update our counts
+            point = f"{force}+{flavor}"
+            point_counts[point] = len(df_point)
+
+        # Save a minimal dataframe of counts for later easy reference
+        df_points = pd.concat(points_dfs)
+        df_points = df_points.drop(columns=["sentence_id", "POS"])
+
+        total = sum(point_counts.values())
+
+        # convert counts to relative frequencies
+        prior = {point: point_counts[point] / total for point in point_counts}
 
     ##########################################################################
     # Save results
     ##########################################################################
-
-    # convert counts to relative frequencies
-    prior = {point: point_counts[point] / total for point in point_counts}
 
     # save prior for experiment
     file_util.save_prior(prior_fn, prior)
