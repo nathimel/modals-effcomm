5dd343eb491f00f0242fc0e154b6ed252780defd
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-02-17 17:20:02 -0800

    small typo + shrink font slightly

--- src/analyze.py
+++ src/analyze.py
@@ -58,7 +58,7 @@ def get_modals_plot(
 
     plot = (
         # Set data and the axes
-        pn.ggplot(data=data, mapping=pn.aes(x="complexity", y="comm_cost"))
+        pn.ggplot(mapping=pn.aes(x="complexity", y="comm_cost"))
         + pn.scale_y_continuous(limits=[0, 1])
         + pn.geom_line(size=1, data=pareto_smoothed)
         + pn.geom_point(  # all langs
@@ -85,7 +85,7 @@ def get_modals_plot(
                 natural_data,
                 pn.aes(label="name"),
                 ha="left",
-                size=7,
+                size=6,
                 nudge_x=1,  # orig 9
             )
         )
